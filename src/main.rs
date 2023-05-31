bd236c045a5567e955df2cdbd07d54e928a83e10
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-05-31 12:29:58 -0700

    Give players ids so people can have same name and not be delete swept

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ fn main() {
 
 #[derive(Default, Clone)]
 struct Player {
+    id: i32,
     name: String,
     score: i32,
 }
 
 impl Player {
-    fn new(player_name: String) -> Self {
+    fn new(player_name: String, player_id: i32) -> Self {
         Player {
+            id: player_id,
             name: player_name.clone(),
             score: 0,
         }
@@ -27,6 +29,7 @@ impl Player {
 
 #[derive(Default)]
 struct CompeteApp {
+    player_count: i32, // kept separate because of IDs
     players: Vec<Player>,
     player_edit_result: String,
 }
@@ -38,7 +41,7 @@ impl CompeteApp {
 
 }
 
-fn deletePlayerByName(vector: &Vec<Player>, player_name: String) -> Vec<Player>{
+fn delete_player_by_name(vector: &Vec<Player>, player_name: String) -> Vec<Player>{
     let mut result: Vec<Player> = vec![];
     for item in vector {
         if item.name != player_name.clone() {
@@ -48,9 +51,19 @@ fn deletePlayerByName(vector: &Vec<Player>, player_name: String) -> Vec<Player>{
     result
 }
 
+fn delete_player_by_id(vector: &Vec<Player>, player_id: i32) -> Vec<Player>{
+    let mut result: Vec<Player> = vec![];
+    for item in vector {
+        if item.id != player_id {
+            result.push(item.clone());
+        }
+    }
+    result
+}
+
 impl eframe::App for CompeteApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let mut players = (&self.players).to_vec();
+        let mut players = self.players.clone();
 
         egui::CentralPanel::default().show(ctx, |cui| {
             cui.heading("Compete-inator");
@@ -59,10 +72,7 @@ impl eframe::App for CompeteApp {
                     pui.horizontal(|hui| {
                         hui.label(player.name.clone());
                         if hui.button("🗑").clicked() {
-                            players = deletePlayerByName(
-                                &(players.clone()),
-                                player.name.clone()
-                            );
+                            players = delete_player_by_id(&(players.clone()), player.id);
                         }
                     });
 
@@ -73,8 +83,12 @@ impl eframe::App for CompeteApp {
                 );
                 if response.lost_focus() && pui.input(|i| i.key_pressed(egui::Key::Enter)) {
                     players.push(
-                        Player::new(self.player_edit_result.clone())
+                        Player::new(
+                            self.player_edit_result.clone(),
+                            self.player_count,
+                        )
                     );
+                    self.player_count += 1;
                     self.player_edit_result = "".to_string();
                 }
             });
